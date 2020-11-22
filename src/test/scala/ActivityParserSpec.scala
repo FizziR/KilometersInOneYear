699e6c5e43c43eb73cb7069767e92fc5310a0756
FizziR <felicitas@maurerhaus.de>
2020-11-22 19:29:51 +0100

    Implementing test for RegExs

--- src/test/scala/ActivityParserSpec.scala
+++ src/test/scala/ActivityParserSpec.scala
@@ -1,7 +1,36 @@
 package model
-import scala.util.parsing.combinator.RegexParsers
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.wordspec.AnyWordSpec
 
 
-class ActivityParserSpec extends RegexParsers {
+class ActivityParserSpec extends AnyWordSpec with Matchers {
 
+  val activityParser = new ActivityParser
+  "An ActivityParser" when{
+    "regular expressions" in{
+      "Feli".matches(activityParser.userName.regex) should be(true)
+      "Feli110".matches(activityParser.userName.regex) should be(false)
+
+      "joggte".matches(activityParser.activityName.regex) should be(true)
+
+      "9.66".matches(activityParser.distance.regex) should be(true)
+
+      "km".matches(activityParser.distanceUnit.regex) should be(true)
+
+      "in".matches(activityParser.In.regex) should be(true)
+
+      "1.5".matches(activityParser.duration.regex) should be(true)
+
+      "h".matches(activityParser.durationUnit.regex) should be(true)
+
+      "mdfjksfhs slfjsdlfjsdlf :".matches(activityParser.text.regex) should be(true)
+
+      "Feli, Lukas, Paul".matches(activityParser.userGroup.regex) should be(true)
+      "Feli, Lukas, Paul,".matches(activityParser.userGroup.regex) should be(false)
+
+    }
+    
+  }
 }
